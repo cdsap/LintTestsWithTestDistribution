c703e7c1b9a2205b72ff23c0ece0b02fcdaf9a6d
Inaki Villar <ivillar@gradle.com>
2026-04-14 09:14:52 -0700

    adding file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,24 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "4.3.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
+}
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { "0.0.0.0" } }
+        }
+    }
+}
+
 include(":app")
 include(":checks")
 include(":library")
